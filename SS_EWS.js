538f9a10114aa2e40896a146400e1712b9d71ca9
Solorio17 <jasonsoloriom@gmail.com>
2017-08-30 22:54:59 -0600

    Adding the nunjucks and consolidate modules for engine, and connecting to the views directory

--- SS_EWS.js
+++ SS_EWS.js
@@ -1,1 +1,20 @@
-var express = require("express");
+const express = require("express"),
+      app = express(),
+      engines = require("consolidate");
+
+app.engine("html", engines.nunjucks);
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.use(function(req, res){
+  res.sendStatus(400);
+});
+
+const server = app.listen(3000, function(){
+  let port = server.address().port;
+  console.log("LISTENING ON PORT %s", port);
+});
