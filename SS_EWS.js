c2c73b8f36bef9481a0ee44715f1429ae3131e83
Solorio17 <jasonsoloriom@gmail.com>
2017-09-01 15:30:03 -0600

    Connecting to public directory, and using JSON for displaying static html files

--- SS_EWS.js
+++ SS_EWS.js
@@ -1,25 +1,43 @@
 const express = require("express"),
       app = express(),
-      engines = require("consolidate");
+      path = require("path"),
+      bodyParser = require("body-parser");
 
-app.engine("html", engines.nunjucks);
-app.set("view engine", "html");
-app.set("views", __dirname + "/views");
+//USING A STATIC PATH
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get("/", (req, res) => {
-res.render("index");
-});
+app.post("/subscribe", (req, res) => {
+ let name = req.body.name;
+ let email = req.body.email;
 
-app.get("/:name", (req, res) => {
-let users = req.params.name;
-res.render("name", {users: users});
-});
+ console.log(name + "has just subscribed with " + email);
 
-app.use((req, res) => {
-  res.sendStatus(400);
-});
+ res.redirect("index.html");
+})
+
 
 const server = app.listen(3000,() => {
   let port = server.address().port;
   console.log("Listening On Port... %s", port);
 });
+
+
+//USING EXPRESS FOR ROUTING AND DISPLAYING
+// engines = require("consolidate"),
+// app.engine("html", engines.nunjucks);
+// app.set("view engine", "html");
+// app.set("views", __dirname + "/views");
+// app.get("/", (req, res) => {
+// res.render("index");
+// });
+//
+// app.get("/:name", (req, res) => {
+// let users = req.params.name;
+// res.render("name", {users: users});
+// });
+//
+// app.use((req, res) => {
+//   res.sendStatus(400);
+// });
